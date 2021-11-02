3b18a414448a1f16eb9e48716f15c1ef3d103537
ByMartrixx <bymartrixx@gmail.com>
2021-11-02 17:50:27 -0300

    Compare files by whole content in BasicTests

--- src/test/java/org/quiltmc/mappings_hasher/BasicTests.java
+++ src/test/java/org/quiltmc/mappings_hasher/BasicTests.java
@@ -4,31 +4,26 @@ import org.junit.jupiter.api.*;
 import picocli.CommandLine;
 
 import java.io.*;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class BasicTests {
     @Test
-    public void hash_1_17_1() throws IOException {
+    public void hash_1_17_1() throws IOException, URISyntaxException {
         int exitCode = new CommandLine(new Main()).execute("--version=1.17.1", "--cache=cache");
         Assertions.assertEquals(0, exitCode);
 
         Path outPath = Paths.get("mappings", "hashed-1.17.1.tiny");
         Assertions.assertTrue(Files.exists(outPath));
-        assertEqualContent(getClass().getResourceAsStream("/mappings/hashed-1.17.1.tiny"), Files.newInputStream(outPath));
+        Path expectedPath = Paths.get(getClass().getResource("/mappings/hashed-1.17.1.tiny").toURI());
+        assertEqualContent(expectedPath, outPath);
     }
 
-    private static void assertEqualContent(InputStream expected, InputStream actual) throws IOException {
-        BufferedInputStream first = new BufferedInputStream(expected);
-        BufferedInputStream second = new BufferedInputStream(actual);
-
-        int firstByte;
-        int secondByte;
-        do {
-            firstByte = first.read();
-            secondByte = second.read();
-            Assertions.assertEquals(firstByte, secondByte);
-        } while (firstByte != -1);
+    private static void assertEqualContent(Path expected, Path actual) throws IOException {
+        String expectedContent = String.join("\n", Files.readAllLines(expected));
+        String actualContent = String.join("\n", Files.readAllLines(actual));
+        Assertions.assertEquals(expectedContent, actualContent, "Content of " + actual + " is not equal to " + expected);
     }
 }
